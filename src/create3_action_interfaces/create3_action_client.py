45f8c3bb9f14b2015c24484f4f6e902966077224
Lance-Town <85811392+Lance-Town@users.noreply.github.com>
2025-04-06 21:23:32 -0700

    add callbacks to client
    - add feedback callback to client
    - add result callback to client

--- src/create3_action_interfaces/create3_action_client.py
+++ src/create3_action_interfaces/create3_action_client.py
@@ -15,16 +15,39 @@ class ChangeStateActionClient(Node):
 
         self._action_client.wait_for_server()
 
-        return self._action_client.send_goal_async(goal_msg)
+        self._send_goal_future = self._action_client.send_goal_async(goal=goal_msg, feedback_callback=self.feedback_callback)
+
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected')
+            return
+        
+        self.get_logger().info('Goal accepted')
+
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+    
+    def get_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info(f'Result {result.success}')
+        rclpy.shutdown()
+
+    def feedback_callback(self, feedback_msg):
+        feedback = feedback_msg.feedback
+        self.get_logger().info(f'Recieved Feedback: {feedback.percent_finished}')
 
 def main(args=None):
     rclpy.init(args=args)
 
     action_client = ChangeStateActionClient()
 
-    future = action_client.send_goal(5)
+    action_client.send_goal(5)
 
-    rclpy.spin_until_future_complete(action_client, future=future)
+    rclpy.spin(action_client)
 
 if __name__ == '__main__':
     main()
