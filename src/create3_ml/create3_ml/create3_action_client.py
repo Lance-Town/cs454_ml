8702e9e2f2f3ddba761e1ac4c41b1f9484f69e24
Lance-Town <85811392+Lance-Town@users.noreply.github.com>
2025-04-08 20:48:35 -0700

    goal queuing works
    - YES
    - SO HAPPY
    - TOOK 5 HOURS TO GET THIS TO WORK
    - One slight bug, for some reason the robot is doing random turning moves. This is a bug I will fix later. Same code, and 50% of the time it will turn one direction, other times it will turn opposite. Weird

--- src/create3_ml/create3_ml/create3_action_client.py
+++ src/create3_ml/create3_ml/create3_action_client.py
@@ -3,72 +3,55 @@ from rclpy.action import ActionClient
 from rclpy.node import Node
 
 from create3_action_interfaces.action import ChangeState
-from action_msgs.msg._goal_status import GoalStatus
 
 class ChangeStateActionClient(Node):
     def __init__(self):
         super().__init__('create3_change_state_action_client')
         self._action_client = ActionClient(self, ChangeState, 'change_state')
 
-    def send_goal(self, state):
+    def send_goal_and_wait(self, state):
         goal_msg = ChangeState.Goal()
         goal_msg.state = state
 
         self._action_client.wait_for_server()
 
-        self._send_goal_future = self._action_client.send_goal_async(goal=goal_msg, feedback_callback=self.feedback_callback)
+        send_goal_future = self._action_client.send_goal_async(
+            goal_msg,
+            feedback_callback=self.feedback_callback
+        )
+        rclpy.spin_until_future_complete(self, send_goal_future)
 
-        self._send_goal_future.add_done_callback(self.goal_response_callback)
-
-        # while not self._send_goal_future.done():
-        #     pass
-        
-        # self._goal_uuid = self._send_goal_future.result()
-
-        # while self._goal_uuid and self._goal_uuid.status == GoalStatus.STATUS_UNKNOWN:
-        #     pass
-
-        # while self._goal_uuid and self._goal_uuid.status is not GoalStatus.STATUS_SUCCEEDED:
-        #     if self._goal_uuid is None or self._goal_uuid.status is GoalStatus.STATUS_CANCELED:
-        #         break
-        #     pass
-        
-        # self._goal_uuid = None
-
-    def goal_response_callback(self, future):
-        goal_handle = future.result()
-
+        goal_handle = send_goal_future.result()
         if not goal_handle.accepted:
-            self.get_logger().info('Goal rejected')
+            self.get_logger().info(f'Goal {state} rejected')
             return
-        
-        self.get_logger().info('Goal accepted')
 
-        self._get_result_future = goal_handle.get_result_async()
-        self._get_result_future.add_done_callback(self.get_result_callback)
-    
-    def get_result_callback(self, future):
-        result = future.result().result
-        self.get_logger().info(f'Result {result.success}')
-        rclpy.shutdown()
+        self.get_logger().info(f'Goal {state} accepted')
+
+        get_result_future = goal_handle.get_result_async()
+        rclpy.spin_until_future_complete(self, get_result_future)
+
+        result = get_result_future.result().result
+        self.get_logger().info(f'Goal {state} result: {result.success}')
 
     def feedback_callback(self, feedback_msg):
         feedback = feedback_msg.feedback
-        self.get_logger().info(f'Recieved Feedback: {feedback.percent_finished}')
+        self.get_logger().info(f'Received Feedback: {feedback.percent_finished}')
 
 def main(args=None):
     rclpy.init(args=args)
 
     action_client = ChangeStateActionClient()
 
-    action_client.send_goal(1)
-    action_client.send_goal(2)
-
-    # rclpy.spin(action_client)
     try:
-        rclpy.spin(action_client)
+        # Goal 1
+        action_client.send_goal_and_wait(1)
+
+        # Goal 2 (only starts after goal 1 is done)
+        action_client.send_goal_and_wait(2)
+
     except KeyboardInterrupt:
-        action_client.get_logger().info('KeyboardInterrup, shutting down.')
+        action_client.get_logger().info('KeyboardInterrupt, shutting down.')
     finally:
         action_client.destroy_node()
         rclpy.shutdown()
