8d1f24f55309eddc809d389bc1b8b09b9b9229e5
Lance-Town <85811392+Lance-Town@users.noreply.github.com>
2025-05-07 09:50:18 -0700

    remove comments
    - remove commented out code
    - control node works 100% for the movements

--- src/create3_ml/create3_ml/create3_control_node.py
+++ src/create3_ml/create3_ml/create3_control_node.py
@@ -1,11 +1,9 @@
 import rclpy
-from create3_action_client import ChangeStateActionClient
-from create3_action_server import ChangeStateActionServer
-from movement import Slash
+from .create3_action_client import ChangeStateActionClient
+from .create3_action_server import ChangeStateActionServer
+from .movement import Slash
 from fanuc2_ml import BeakerActionClient, BunsenActionClient
-from stdbot_trigger_client import TriggerClient
-
-import time
+from .stdbot_trigger_client import TriggerClient
 
 def main():
     rclpy.init()
@@ -22,16 +20,8 @@ def main():
     dave_tc.send_request()
     print("paul is done")
 
-    # send goal to carson
-    # beaker_ac.send_goal_and_wait(2) # send carson to pick up from paul
-
     # move to grant
     create3_ac.send_goal_and_wait(2)
-
-    # send grant the status code to move the gripper back
-    # bunsen_ac.send_goal_and_wait(4)
-
-    # create3_ac.send_goal_and_wait(3)
 
     rclpy.shutdown()
     
