b6678cd80b8de490777cc0161a39aec24eacce21
Lance-Town <85811392+Lance-Town@users.noreply.github.com>
2025-05-14 21:42:31 -0700

    remove fanuc test code
    - remove code associated with the two FANUC CRX-10 portion of robot line.

--- src/create3_ml/create3_ml/create3_control_node.py
+++ src/create3_ml/create3_ml/create3_control_node.py
@@ -1,15 +1,11 @@
 import rclpy
 from .create3_action_client import ChangeStateActionClient
 from .create3_action_server import ChangeStateActionServer
-from .movement import Slash
-from fanuc2_ml import BeakerActionClient, BunsenActionClient
 from .stdbot_trigger_client import TriggerClient
 
 def main():
     rclpy.init()
     create3_ac = ChangeStateActionClient()
-    beaker_ac = BeakerActionClient()
-    bunsen_ac = BunsenActionClient()
 
     print("Sending iCreate3 the goal")
     create3_ac.send_goal_and_wait(1)
@@ -20,7 +16,6 @@ def main():
     dave_tc.send_request()
     print("paul is done")
 
-    # move to grant
     create3_ac.send_goal_and_wait(2)
 
     rclpy.shutdown()
